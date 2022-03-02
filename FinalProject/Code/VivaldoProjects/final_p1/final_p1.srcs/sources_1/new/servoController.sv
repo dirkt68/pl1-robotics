51e87fd6ce691747307468e4c79007b0bc357cec
Dirk Thieme <dirkt6868@gmail.com>
2022-03-02 12:31:23 -0600

    cleaned up gitlab

---
+++ FinalProject/Code/VivaldoProjects/final_p1/final_p1.srcs/sources_1/new/servoController.sv
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+
+module servoController(
+        input clk100,
+        input [2:0] sw,
+        input reset,
+        output servoOut
+    );
+
+    // initalize a max of 1KHz counter, width, and temporary in-between register for the output
+    logic [17:0] max1kclk = 0;
+    logic [17:0] width = 0;
+    logic servoOutTemp = 0;
+
+    // check which switch is pulled to determine which position the servo sits at (TEMPORARY)
+    always @(posedge clk100) begin
+        case (sw)
+            3'b001: width <= 50_000;  // 500Hz
+            3'b010: width <= 75_000;  // 750Hz
+            3'b100: width <= 100_000; // 1000Hz
+            default: width <= 30_000; // 300Hz
+        endcase
+    end
+
+    // begin outputting to the servo, if reset is triggered skip always block
+    always @(posedge clk100 iff reset == 0) begin
+        if (max1kclk == 100_000) begin
+            max1kclk <= 0;
+        end
+        max1kclk <= max1kclk + 1;
+
+        if (max1kclk < width) begin
+            servoOutTemp <= 1;
+        end
+        else begin
+            servoOutTemp <= 0;
+        end
+    end
+    
+    assign servoOut = servoOutTemp;
+
+endmodule
---
+++ FinalProject/Code/VivaldoProjects/final_p1/final_p1.srcs/sources_1/new/sevseg_controller.sv
@@ -0,0 +1,64 @@
+`timescale 1ns/1ps
+
+module sevseg_controller(
+    input clk100,
+    input [1:0] sw,
+    output [6:0] seg,
+    output [3:0] an
+);
+
+    // create enumerators for anode position and switch position
+    typedef enum {FOUR = 0, THREE = 1, TWO = 2, ONE = 3} ANODE_POS;
+    typedef enum {DASH = 0, FRIEND = 1, ENEMY = 2} SWITCH_POS;
+
+    // create one object for each
+    ANODE_POS position;
+    SWITCH_POS switch;
+
+    // initalize a 20 bit counter and in-between registers for the outputs
+    logic [19:0] counter = 0;
+    logic [6:0] seg_temp;
+    logic [3:0] an_temp;
+
+    // initalize position and switch with default values
+    initial begin
+        position = FOUR;
+        switch = DASH;
+    end
+
+    // increment the counter
+    always @(posedge clk100) begin
+        counter <= counter + 1;
+    end
+
+    // cast the number in the switch input to its corresponding enum value
+    assign switch = SWITCH_POS'(sw);
+    assign position = ANODE_POS'(counter[19:18]);
+
+    // determine which anode to activate based on position
+    always @(posedge clk100) begin
+        case (position)
+            FOUR:    an_temp <= 4'b0111;
+            THREE:   an_temp <= 4'b1011;
+            TWO:     an_temp <= 4'b1101;
+            ONE:     an_temp <= 4'b1110;
+            default: an_temp <= 4'b0111;
+        endcase
+    end
+    
+    // output the anode
+    assign an = an_temp;
+
+    // determine what to output based on switches pulled (TEMPORARY)
+    always @(posedge clk100) begin
+        case (switch)
+            DASH:   seg_temp <= 7'b0111111;
+            FRIEND: seg_temp <= 7'b0001110;
+            ENEMY:  seg_temp <= 7'b0000110;
+            default: seg_temp <= 7'b0111111;
+        endcase
+    end
+    // output segments
+    assign seg = seg_temp;
+    
+endmodule
